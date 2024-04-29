b81cccece4d04eb5408bf8a3baa3be7d434e1f5a
charlinedev <79838535+charlinedev@users.noreply.github.com>
2024-04-29 17:43:37 +0200

    update nav and start footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content:  [ "./_layout/*.html",
+              "./_site/*.html",
               "./_site/**/*.html",
               "./_posts/*.{html,md}",
               "./_includes/*.html",
@@ -9,10 +10,15 @@ module.exports = {
               "./autors.html", 
               "./tags.html"],
   theme: {
+    fontFamily: {
+        'sans': ['Noyh Geometric']
+    },
     colors: {
-        'navysciam': '#384a72',
-        'yellowsciam': '#ffec3f',
-        'light-blue': '#9abcc3'
+        navysciam: '#384a72',
+        yellowsciam: '#ffec3f',
+        lightbluesciam: "#9abcc3",
+        greysciam: "#9CA3AF"
+        
       }, 
     extend: {
       
