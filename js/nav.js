7be6231590b14403610133d33f2a6d0b60092cac
charlinedev <79838535+charlinedev@users.noreply.github.com>
2024-05-14 17:44:01 +0200

    modif btn tags

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,5 @@
+
+// Ouverture bouton de menu de navigation
 document.addEventListener("DOMContentLoaded", function() {
     const toggleButton = document.querySelector('.menu-button');
     const mobileMenu = document.getElementById('mobile-menu');
@@ -10,5 +12,42 @@ document.addEventListener("DOMContentLoaded", function() {
       mobileMenu.classList.toggle('hidden');
       openIcon.classList.toggle('hidden');
       closeIcon.classList.toggle('hidden');
+    });
+  });
+
+  // Ouverture et fermeture bouton menu générique
+  document.addEventListener("DOMContentLoaded", function() {
+    const selectButtons = document.querySelectorAll('.select-button');
+
+    selectButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const menuId = button.getAttribute('aria-controls');
+            const menu = document.getElementById(menuId);
+            const isExpanded = button.getAttribute('aria-expanded') === 'true';
+            button.setAttribute('aria-expanded', !isExpanded);
+            menu.classList.toggle('hidden');
+
+            // Gestion des icônes de menu ouvert/fermé
+            const openIcon = button.querySelector('.menu-open-icon');
+            const closeIcon = button.querySelector('.menu-close-icon');
+            if (openIcon && closeIcon) {
+                openIcon.classList.toggle('hidden');
+                closeIcon.classList.toggle('hidden');
+              }
+          });
+      });
+  });
+
+  // Filter et rechercher dans le bouton menu générique
+  document.addEventListener('DOMContentLoaded', function() {
+    const tagFilter = document.querySelector('.tag-filter');
+    const tagLinks = document.querySelectorAll('#tag-menu a');
+
+    tagFilter.addEventListener('input', function() {
+      const search = tagFilter.value.toLowerCase();
+      tagLinks.forEach(link => {
+        const tagText = link.textContent.toLowerCase();
+        link.style.display = tagText.includes(search) ? '' : 'none';
+      });
     });
   });
