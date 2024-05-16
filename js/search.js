94433e71239d90add0aa0d914d0e1e5e9842cf19
charlinedev <79838535+charlinedev@users.noreply.github.com>
2024-05-16 17:11:31 +0200

    update lunr

--- js/search.js
+++ js/search.js
@@ -1,26 +1,61 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('/search.json')
-      .then(response => response.json())
-      .then(data => {
-        var idx = lunr(function () {
-          this.ref('id')
-          this.field('title')
-          this.field('content')
+(function() {
+    function displaySearchResults(results, store) {
+      var searchResults = document.getElementById('search-results');
   
-          data.forEach(function (doc) {
-            this.add(doc)
-          }, this)
-        });
+      if (results.length) { 
+        var appendString = '';
   
-        // Fonction de recherche
-        function search(query) {
-          return idx.search(query).map(result => {
-            return data.find(d => d.id === result.ref);
-          });
+        for (var i = 0; i < results.length; i++) {
+          var item = store[results[i].ref];
+          appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
+          appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
         }
   
-        // Exemple d'utilisation
-        var results = search('spring');  // Remplacez 'exemple' par votre terme de recherche
-        console.log(results);  // Affiche les résultats de recherche dans la console
+        searchResults.innerHTML = appendString;
+      } else {
+        searchResults.innerHTML = '<li>No results found</li>';
+      }
+    }
+  
+    function getQueryVariable(variable) {
+      var query = window.location.search.substring(1);
+      var vars = query.split('&');
+  
+      for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+  
+        if (pair[0] === variable) {
+          return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+        }
+      }
+    }
+  
+    var searchTerm = getQueryVariable('query');
+  
+    if (searchTerm) {
+      document.getElementById('search-box').setAttribute("value", searchTerm);
+  
+      // Initalize lunr with the fields it will be searching on. I've given title
+      // a boost of 10 to indicate matches on this field are more important.
+      var idx = lunr(function () {
+        this.field('id');
+        this.field('title', { boost: 10 });
+        this.field('author');
+        this.field('category');
+        this.field('content');
       });
-  });
+  
+      for (var key in window.store) { // Add the data to lunr
+        idx.add({
+          'id': key,
+          'title': window.store[key].title,
+          'author': window.store[key].author,
+          'category': window.store[key].category,
+          'content': window.store[key].content
+        });
+  
+        var results = idx.search(searchTerm); // Get lunr to perform a search
+        displaySearchResults(results, window.store); // We'll write this in the next section
+      }
+    }
+  })();
