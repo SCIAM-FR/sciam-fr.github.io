62c0dd8d8ba3d0d95b8a7ca8c80b1afea3afe676
Houra-Am <houra.am@gmai.com>
2024-06-06 15:42:33 +0200

    remove unused script

--- js/scroll.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', function() {
-    const image = document.getElementById('backgroundImage');
-    const baseScale = 0.95; // Initial smaller scale
-    const scrollScaleIncrease = window.scrollY / 3000; // Increased sensitivity
-    const newScale = Math.min(baseScale + scrollScaleIncrease, 1.10); // Capping the maximum scale to 1.10
-    image.style.transform = `scale(${newScale})`;
-  });
