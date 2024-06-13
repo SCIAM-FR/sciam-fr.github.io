671849e6898da3f88fe068074caa070e0b9dbb1f
charlinedev <79838535+charlinedev@users.noreply.github.com>
2024-06-13 09:01:15 +0200

    adjusted navbar - slide home autoplay

--- js/nav.js
+++ js/nav.js
@@ -85,6 +85,11 @@ document.addEventListener("DOMContentLoaded", function() {
     new Swiper('.mySwiper', {
       slidesPerView: 1,
       spaceBetween: 10,
+      loop: true,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
