03de4e50f7f9a6f8d08a5d2b3343d703cd285766
Houra-Am <houra.am@gmai.com>
2024-06-26 15:21:07 +0200

    Post styling improvements

--- typography-preset.js
+++ typography-preset.js
@@ -8,44 +8,70 @@ module.exports = {
               css: { // Override asciidoc default styles
                 maxWidth: '2000px',
 
+                'h1, h2, h3, h4, h5, h6': {
+                  fontFamily: 'Noyh Geometric',
+                  color: `${theme('colors.navysciam')} !important`,
+                  marginTop:'24px !important',
+                  marginBottom:'24px !important',
+                },
+
                 'h1': {
                   fontSize: '2.25rem !important',
-                  lineHeight: '2.5rem !important',
-                  // margin: '1em 0px',
-                  color: `${theme('colors.navysciam')} !important`,
+                  lineHeight: '2.25rem !important',
                 },
                 'h2': {
                   fontSize: '1.875rem !important',
                   lineHeight: '2.25rem !important',
-                  // margin: '1em 0px',
-                  color: `${theme('colors.navysciam')} !important`,
                 },
                 'h3': {
                   fontSize: '1.5rem !important',
                   lineHeight: '2rem !important',
-                  // margin: '1em 0px',
-                  color: `${theme('colors.navysciam')} !important`,
                 },
                 'h4': {
                   fontSize: '1.25rem !important',
                   lineHeight: '1.75rem !important',
-                  // margin: '1em 0px',
-                  color: `${theme('colors.navysciam')} !important`,
                 },
 
                 '#preamble > .sectionbody > .paragraph:first-of-type > p': {
                   fontSize: '1.25rem !important',
                   lineHeight: '1.7rem !important',
-                  fontWeight: '500 !important',
+                  fontWeight: '600 !important',
                   marginBottom:'1.2rem !important',
                   color: `${theme('colors.navysciam')} !important`,
+                },
+
+                'p': {
+                  fontSize: '1.25rem !important',
+                  marginTop:'0.25rem !important',
+                  marginBottom:'0.25rem !important',
+                },
+
+                'a': {
+                  color: theme('colors.blue.600'),
+                        '&:hover': {
+                        color: theme('colors.yellow.500'),
+                  },
+                },
+
+                'code': {
+                  fontFamily: 'Droid Sans Mono',
+                  fontSize: '1rem !important',
+                  lineHeight: '1.2rem !important',
+                },
+
+                'table': {
+                  width: '100%', 
+                  verticalAlign: 'middle',
+                },
+                'th, td': {
+                  verticalAlign: 'middle',
                 },
 
                 'h2 a': { 
                   fontSize: '2.25rem !important',
                   lineHeight: '2.5rem !important',
                   textDecoration: 'none !important',
-                  fontFamily: '"Noyh Geometric" sans-serif !important',
+                  fontFamily: '"Noyh Geometric" !important',
                   fontWeight: 'bold !important',
                   color: `${theme('colors.navysciam')} !important`,
                   '&:hover': {
@@ -56,7 +82,7 @@ module.exports = {
                   fontSize: '1.875rem !important',
                   lineHeight: '2.25rem !important',
                   textDecoration: 'none !important',
-                  fontFamily: '"Noyh Geometric" sans-serif !important',
+                  fontFamily: '"Noyh Geometric" !important',
                   fontWeight: '600 !important',
                   color: `${theme('colors.navysciam')} !important`,
                   '&:hover': {
@@ -67,7 +93,7 @@ module.exports = {
                   fontSize: '1.5rem !important',
                   lineHeight: '2rem !important',
                   textDecoration: 'none !important',
-                  fontFamily: '"Noyh Geometric" sans-serif !important',
+                  fontFamily: '"Noyh Geometric" !important',
                   fontWeight: '600 !important',
                   color: `${theme('colors.navysciam')} !important`,
                   '&:hover': {
@@ -216,28 +242,15 @@ module.exports = {
                   },
                 },
 
-                // '.paragraph> p': {
-                //   marginTop: '1rem !important',
-                //   marginBottom: '1rem !important',
-                // },
-    
-                // 'p code': {  
-                //   fontFamily: '"Droid Sans Mono" monospace !important',
-                //   fontSize: '1rem !important', 
-                //   lineHeight: '1.75rem !important',
-                //   fontWeight: 'normal !important'
-                // },
                 '.paragraph> p code': {  
                   fontFamily: '"Droid Sans Mono" monospace !important',
                   fontSize: '1rem !important',
-                  // lineHeight: '1.75rem !important', 
                   fontWeight: 'normal !important'
                 },
                 'code' : {
                   color: `${theme('colors.navysciam')} !important`,
                   fontFamily: '"Droid Sans Mono" monospace !important',
                   fontSize: '1.1rem',
-                  // lineHeight: '1.75rem !important', 
                   fontWeight: 'normal !important',
                 },
                 'p code::before': {
@@ -250,6 +263,20 @@ module.exports = {
                   content: 'none',
                 },
                 'h2 code::after, h3 code::after, h4 code::after': {
+                  content: 'none',
+                },
+
+                'table td.content code::before' : {
+                  content: 'none',
+                },
+                'table td.content code::after' : {
+                  content: 'none',
+                },
+
+                '.title code::before' : {
+                  content: 'none',
+                },
+                '.title code::after' : {
                   content: 'none',
                 },
 
@@ -271,22 +298,40 @@ module.exports = {
                   fontWeight: 'normal !important',
                 },
 
+                '.listingblock > .content > pre:not(.highlight)': {
+                  color: `${theme('colors.navysciam')} !important`,
+                  fontSize: '0.875rem !important',
+                },
+
+                '.literalblock pre, .listingblock > .content > pre': {
+                  fontFamily: '"Droid Sans Mono" !important',
+                  fontSize: '1rem !important',
+                },
+
                 '.admonitionblock td.content > .title, .audioblock > .title, .exampleblock > .title, .imageblock > .title, .listingblock > .title, .literalblock > .title, .stemblock > .title, .openblock > .title, .paragraph > .title, .quoteblock > .title, table.tableblock > .title, .verseblock > .title, .videoblock > .title, .dlist > .title, .olist > .title, .ulist > .title, .qlist > .title, .hdlist > .title': {
-                    fontFamily: `"Noyh Geometric", sans-serif !important`,
-                    fontSize: '1.25rem !important',
-                    lineHeight: '1.75rem !important',
-                    fontStyle: 'italic !important',
-                    fontWeight: 'normal !important',
-                    color: theme('colors.navysciam') + ' !important',
+                  fontSize: '1.125rem !important',  
+                  fontFamily: '"Noyh Geometric" !important',
+                  color: `${theme('colors.navysciam')} !important`,
                 },
 
-                '.admonitionblock > table td.content': {
-                    display: 'inline-block !important',
-                    verticalAlign: 'middle !important',
+                'admonitionblock note td.content > .title code': {
+                  fontSize: '0.875rem !important',  
+                  fontFamily: '"Droid Sans Mono" monospace !important',
                 },
 
-                '.admonitionblock > table td.icon': {
-                    width: '0 !important',
+                '.listingblock> .title code': {
+                  fontSize: '0.875rem !important',
+                },
+
+                'table > tbody > tr > td code': {
+                  fontSize: '0.875rem !important',
+                },
+
+                'table > thead > tr > th': {
+                  textAlign: 'center !important',
+                },
+                'table > tbody > tr > td p': {
+                  textAlign: 'center',
                 },
 
                 '.content> pre': {
@@ -295,12 +340,26 @@ module.exports = {
                     overflowX: 'auto',
                     display: 'block',
                     border: `1px solid ${theme('colors.gray.200')}`,
+                    marginTop:'1rem !important',
+                    marginBottom:'1rem !important',
                 },
 
                 '.content> pre span': {
-                  fontSize: '1rem !important',
-                  fontWeight: 'normal !important',
                   fontFamily: '"Droid Sans Mono" monospace !important',
+                },
+
+                'td code::before': {
+                  content: 'none',
+                },
+                'td code::after': {
+                  content: 'none',
+                },
+
+                'td.content': {
+                  fontSize: '1.125rem !important',
+                },
+                'td.content code': {
+                  fontSize: '1rem !important',
                 },
 
                 img: {
@@ -310,7 +369,7 @@ module.exports = {
                 },
               },
             },
-          }),
+      }), 
       },
     },
     plugins: [
